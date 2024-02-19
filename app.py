01e274fc12e193baef99b83e2e8fb2f6a3a33364
xpc031019 <160452075+xpc031019@users.noreply.github.com>
2024-02-20 01:32:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask（——name——）
 
 @app.route（“/”，methods=[“GET","POST"])
 def index（）:
-return（render_template（“index.html”）
+return（render_template（“index.html”))
 
 if——name——==“——main——”：
 app.run（）
